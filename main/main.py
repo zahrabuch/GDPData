787cbe629e583162fd75f132b4309d1983394c42
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2026-06-29 19:01:00 -0500

    failed regression, r^2 was .31

--- main/main.py
+++ main/main.py
@@ -5,6 +5,9 @@ import seaborn as sns
 from matplotlib import gridspec
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 file_path = 'country_data.csv'
 
@@ -20,8 +23,39 @@ print(df.describe())
 
 names = ["name", "region", "capital", "currency", "iso2"]
 
-df = df.drop(columns=names)
-corrmat = df.corr()
+df_num = df.drop(columns=names)
+"""
+corrmat = df_num.corr()
 fig = plt.figure(figsize=(12, 9))
 sns.heatmap(corrmat, vmax=.8, square=True)
 plt.show()
+"""
+data = df_num[['gdp', 'imports', 'exports', 'tourists', 'surface_area']]
+print(data.isna().sum())
+data = data.dropna(subset=['gdp'])
+
+cols = ['imports', 'exports', 'tourists', 'surface_area']
+
+for col in cols:
+    data[col] = data[col].fillna(df[col].median())
+
+print(data.isna().sum())
+"""
+corrmat = train.corr()
+fig = plt.figure(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True)
+plt.show()
+"""
+y = np.log(data['gdp'])
+x = data[['imports', 'exports', 'tourists', 'surface_area']]
+
+x_train, x_test, y_train, y_test = train_test_split(
+  x, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(x_train, y_train)
+predictions = model.predict(x_test)
+from sklearn.metrics import r2_score
+score = r2_score(y_test, predictions)
+print(score)
